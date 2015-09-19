8807feaf8e77a4218d28b2f3e6b0442c2db3a773
Jan Svoboda <jan@mluv.cz>
2015-09-19 22:24:52 +0200

    fix reordering errors in acts_as_list plugin

--- lib/plugins/acts_as_list/lib/active_record/acts/list.rb
+++ lib/plugins/acts_as_list/lib/active_record/acts/list.rb
@@ -178,17 +178,17 @@ module ActiveRecord
         # Return the next higher item in the list.
         def higher_item
           return nil unless in_list?
-          acts_as_list_class.where(
-            "#{scope_condition} AND #{position_column} = #{(send(position_column).to_i - 1).to_s}"
-          ).first
+          acts_as_list_class.where(scope_condition).
+            where(position_column => send(position_column).to_i - 1).
+            first
         end
 
         # Return the next lower item in the list.
         def lower_item
           return nil unless in_list?
-          acts_as_list_class.where(
-            "#{scope_condition} AND #{position_column} = #{(send(position_column).to_i + 1).to_s}"
-          ).first
+          acts_as_list_class.where(scope_condition).
+            where(position_column => send(position_column).to_i + 1).
+            first
         end
 
         # Test if this record is in a list
@@ -235,38 +235,38 @@ module ActiveRecord
           # This has the effect of moving all the higher items up one.
           def decrement_positions_on_higher_items(position)
             acts_as_list_class.
-              where("#{scope_condition} AND #{position_column} <= #{position}").
-              update_all("#{position_column} = (#{position_column} - 1)")
+              where(scope_condition).where(acts_as_list_class.arel_table[position_column.to_sym].lte(send(position_column))).
+              decrement_counter(position_column, 1)
           end
 
           # This has the effect of moving all the lower items up one.
           def decrement_positions_on_lower_items
             return unless in_list?
             acts_as_list_class.
-              where("#{scope_condition} AND #{position_column} > #{send(position_column).to_i}").
-              update_all("#{position_column} = (#{position_column} - 1)")
+              where(scope_condition).where(acts_as_list_class.arel_table[position_column.to_sym].gt(send(position_column))).
+              decrement_counter(position_column, 1)
           end
 
           # This has the effect of moving all the higher items down one.
           def increment_positions_on_higher_items
             return unless in_list?
             acts_as_list_class.
-              where("#{scope_condition} AND #{position_column} < #{send(position_column).to_i}").
-              update_all("#{position_column} = (#{position_column} + 1)")
+              where(scope_condition).where(acts_as_list_class.arel_table[position_column.to_sym].lt(send(position_column))).
+              increment_counter(position_column, 1)
           end
 
           # This has the effect of moving all the lower items down one.
           def increment_positions_on_lower_items(position)
             acts_as_list_class.
-              where("#{scope_condition} AND #{position_column} >= #{position}").
-              update_all("#{position_column} = (#{position_column} + 1)")
+              where(scope_condition).where(acts_as_list_class.arel_table[position_column.to_sym].gte(send(position_column))).
+              increment_counter(position_column, 1)
           end
 
           # Increments position (<tt>position_column</tt>) of all items in the list.
           def increment_positions_on_all_items
             acts_as_list_class.
-              where("#{scope_condition}").
-              update_all("#{position_column} = (#{position_column} + 1)")
+              where(scope_condition).
+              increment_counter(position_column, 1)
           end
 
           def insert_at_position(position)
